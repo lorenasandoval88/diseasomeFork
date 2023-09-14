3e4d52668bb0cfb7b5a4a1cf6cefc7f3c57493d0
lorenasandoval88 <43822820+lorenasandoval88@users.noreply.github.com>
2023-09-14 11:22:00 -0400

    updated calc

--- pgs23.js
+++ pgs23.js
@@ -1,6 +1,6 @@
 (async()=>{
-    //pgs23 = await import('https://github.com/lorenasandoval88/diseasomes/export.js')
-    pgs23 = await import('http://127.0.0.1:5501/export.js')
+    pgs23 = await import('https://github.com/epishpere/riskome/export.js')
+    //pgs23 = await import('http://127.0.0.1:5500/export.js')
     if(typeof(define)!='undefined'){
         define(pgs23)
     }
