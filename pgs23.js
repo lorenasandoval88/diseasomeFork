fb9d7d5c554eba6f5213abc530ab4f76d9e770b0
lorenasandoval88 <43822820+lorenasandoval88@users.noreply.github.com>
2023-09-14 11:36:52 -0400

    edit padding

--- pgs23.js
+++ pgs23.js
@@ -1,5 +1,5 @@
 (async()=>{
-    pgs23 = await import('https://github.com/epishpere/riskome/export.js')
+    pgs23 = await import('https://github.com/episphere/riskome/export.js')
     //pgs23 = await import('http://127.0.0.1:5500/export.js')
     if(typeof(define)!='undefined'){
         define(pgs23)
