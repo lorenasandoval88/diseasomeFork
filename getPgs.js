f8868b440d6f976dc8eac0608175be9c22af341f
lorenasandoval88 <43822820+lorenasandoval88@users.noreply.github.com>
2024-01-19 11:55:34 -0500

    Update getPgs.js

--- getPgs.js
+++ getPgs.js
@@ -179,8 +179,9 @@ async function getPGSbyTrait(trait, traitFiles,scoringFiles) {
 }
 async function getPGSidsForOneTrait(traitData,trait,traits, traitFiles,scoringFiles) {
     let obj = {}
-    let traitData2 = traitData[trait].pgsInfo
     console.log("traitData[trait].pgsInfo",traitData[trait].pgsInfo)
+
+    let traitData2 = traitData[trait].pgsInfo
             .filter( x =>  x.variants_number < 1000 & x.variants_number > 120)
             .map( x => x.id)
     obj["Ids"] = traitData2
